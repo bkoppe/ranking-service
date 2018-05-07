af9d2b813c626f2fa21353575ec0cf9fe4051256
bkoppe <benni@koppe-mail.de>
2018-05-07 21:47:21 +0200

    create boot project

---
+++ src/main/java/de/augsburg1871/handball/rankingservice/RankingServiceApplication.java
@@ -0,0 +1,12 @@
+package de.augsburg1871.handball.rankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingServiceApplication.class, args);
+	}
+}
